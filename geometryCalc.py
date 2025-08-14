5b4d6057c89147b7f4779cc2af93b0c2a4034d70
erpostgr25 <er.shashi09@outlook.com>
2025-08-14 18:05:36 +0530

    Fixed unwanted values

--- geometryCalc.py
+++ geometryCalc.py
@@ -12,7 +12,7 @@ radius = 10
 print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
 #width = 6
 #print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
-=======
+
 #radius = 5
 #print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
 length = 15
